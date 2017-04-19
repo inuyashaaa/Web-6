c25c981c2b2cd3e7a8ba968b7dc6824db1813852
longlk-PC\longlk <darknesszx@gmail.com>
2017-04-19 18:25:34 +0700

    prepare be2

--- BE1/app.js
+++ BE1/app.js
@@ -3,32 +3,61 @@ console.log('Hello nodemon');
 const fs = require('fs');
 //dung cai thu vien express
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const imagesController = require(__dirname + '/modules/images/imagesController');
 
 var app = express();
 
 //set public folder public
-//app.use(urlencoded)
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json({ extended : true}));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
   res.send('./public/index.html');
 })
 
-app.get('/image/add', (req, res) => {
+app.post('/image', (req, res) => {
+  //doc du lieu tu cai file imageData
+  var imageInfoCollection = imagesController.fetchImageCollection();
+
   //khai bao object
   var imageInfo = {
-    name : req.query.name,
-    imageLink : req.query.imageLink,
-    description : req.query.description
+    name : req.body.name,
+    imageLink : req.body.imageLink,
+    description : req.body.description
   }
+
+  console.log('post data ',req.body);
+
+  //push data moi vao collection
+  imageInfoCollection.push(imageInfo);
+
   //luu lai vao file
-  fs.writeFileSync('imageData.json', JSON.stringify(imageInfo));
+  imagesController.saveImageCollection(imageInfoCollection);
   //bao thanh cong
   res.send('Success');
 })
 
-app.get('image/get', (req,res) => {
-  
+app.get('/image', (req,res) => {
+  var imageInfoCollection = imagesController.fetchImageCollection();
+
+  var htmlString = '';
+
+  imageInfoCollection.forEach((data) => {
+    htmlString += `<div>${data.name}</div><img src="${data.imageLink}"><div>${data.description}</div>`;
+  })
+
+  res.send(htmlString);
+})
+
+app.put('/image', (req, res) => {
+  console.log(req.body);
+})
+
+app.delete('/image', (req, res) => {
+  console.log(req.body);
 })
 
 //mo 1 cai port de chay local
